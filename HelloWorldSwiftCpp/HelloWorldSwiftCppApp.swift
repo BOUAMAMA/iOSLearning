c30f0d7ce9fd83cfc244d92927a90f2d8a272a38
imane <imane@armonicos.co.jp>
2021-07-08 17:37:29 +0900

    Initial Commit

---
+++ HelloWorldSwiftCpp/HelloWorldSwiftCppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldSwiftCppApp.swift
+//  HelloWorldSwiftCpp
+//
+//  Created by imane on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldSwiftCppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
